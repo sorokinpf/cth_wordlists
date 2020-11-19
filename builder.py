45c09e6bad7dde00272e983b37880529a3910ff1
Pavel Sorokin <p.sorokin@bi.zone>
2020-11-19 13:25:49 +0300

    +local files

--- builder.py
+++ builder.py
@@ -4,20 +4,25 @@ import itertools
 import argparse
 
 def parse(val_name,schema):
-    print (val_name)
+    #print (val_name)
     vals = schema[val_name]
     result = []
     for v in vals:
-        print (type(v))
         sub_vals = re.findall('%([^;]+);',v)
         if len(sub_vals)==0:
             result.append(v)
             continue
         sub_arrays = []
+        sub_vals_uniq = []
         for sub_val in sub_vals:
+            if sub_val in sub_vals_uniq:
+                continue
+            sub_vals_uniq.append(sub_val)
             sub_arrays.append(parse(sub_val,schema))
-
+        #print (sub_arrays)
+        #print (sub_vals_uniq)
         possible_values = itertools.product(*sub_arrays)
+
         for x in possible_values:
             generating_v = v
             for sub_val_name,sub_val_value in zip(sub_vals,x):
@@ -38,7 +43,7 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	all_data = safe_load(open(args.source).read())
-	print (all_data)
+	#print (all_data)
 	target = parse(args.target,all_data)
 	save(args.output,target)
 
--- dirsearch/build.sh
+++ dirsearch/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+wget https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt -O dirsearch.txt
 cp dirsearch.txt full.txt
 cat manual.txt >> full.txt
 cat years/years.txt >> full.txt
