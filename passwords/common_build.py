04455d7ed5cd65635af543c7321adbfb73ad5738
Pavel Sorokin <p.sorokin@bi.zone>
2021-02-18 12:15:00 +0300

    lowercase passwords

--- passwords/common_build.py
+++ passwords/common_build.py
@@ -1,8 +1,9 @@
 month = ['Zydfhm','Atdhfkm','Vfhn','Fghtkm','Vfq','B.ym','B.km','Fduecn','Ctynz,hm','Jrnz,hm','Yjz,hm','Ltrf,hm']
 seasons = ['Dtcyf','Ktnj','Jctym','Pbvf']
-
+month_l = [m.lower() for m in month]
+seasons_l = [s.lower() for s in seasons]
 years = range(2017,2022)
-for word in month+seasons:
+for word in month+seasons+month_l+seasons_l:
     for year in years:
         year = str(year)
         print (word+year)
