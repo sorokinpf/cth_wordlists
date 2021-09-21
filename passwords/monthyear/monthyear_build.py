fb5c71893a64397e96a287a078b4b6cad969aae6
Pavel Sorokin <p.sorokin@bi.zone>
2021-09-21 18:53:51 +0300

    Osen and etc...

--- passwords/monthyear/monthyear_build.py
+++ passwords/monthyear/monthyear_build.py
@@ -1,8 +1,8 @@
-month = ['Zydfhm','Atdhfkm','Vfhn','Fghtkm','Vfq','B.ym','B.km','Fduecn','Ctynz,hm','Jrnz,hm','Yjz,hm','Ltrf,hm']
-seasons = ['Dtcyf','Ktnj','Jctym','Pbvf']
+month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'Zydfhm','Atdhfkm','Vfhn','Fghtkm','Vfq','B.ym','B.km','Fduecn','Ctynz,hm','Jrnz,hm','Yjz,hm','Ltrf,hm']
+seasons = ['Leto', 'Summer', 'Zima', 'Winter', 'Osen', 'Autumn', 'Vesna', 'Spring', 'Dtcyf','Ktnj','Jctym','Pbvf']
 month_l = [m.lower() for m in month]
 seasons_l = [s.lower() for s in seasons]
-years = range(2017,2022)
+years = range(2018,2023)
 for word in month+seasons+month_l+seasons_l:
     for year in years:
         year = str(year)
